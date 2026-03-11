10306537088448b71fc3abb2a6f5a7ed195957b4
Aishwarya Kumar <aishwaryaksharma@gmail.com>
2026-03-11 11:09:55 +0530

    Basic server running happily

---
+++ shortener/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ShortenURLView, redirect_to_full_url
+
+urlpatterns = [
+    path('api/shorten/', ShortenURLView.as_view(), name='shorten'),
+    path('<str:short_code>/', redirect_to_full_url, name='redirect'),
+]
--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404, redirect
+from .models import URLMapping
 
-# Create your views here.
+class ShortenURLView(APIView):
+    def post(self, request):
+        original_url = request.data.get('url')
+        mapping = URLMapping.objects.create(original_url=original_url)
+        return Response({
+            "short_url": f"http://localhost:8000/{mapping.short_code}"
+        }, status=status.HTTP_201_CREATED)
+
+def redirect_to_full_url(request, short_code):
+    mapping = get_object_or_404(URLMapping, short_code=short_code)
+    return redirect(mapping.original_url)
